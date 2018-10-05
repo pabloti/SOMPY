d77c9fe89ec47744d929e564856a872362d080c1
pabloti <39644754+pabloti@users.noreply.github.com>
2018-10-05 20:04:44 +0200

    Update mapview.py

--- sompy/visualization/mapview.py
+++ sompy/visualization/mapview.py
@@ -114,7 +114,7 @@ class View2DPacked(MapView):
     def _set_axis(self, ax, msz0, msz1):
         plt.axis([0, msz0, 0, msz1])
         plt.axis('off')
-        ax.axis('off')
+        #ax.axis('off')
 
     def show(self, som, what='codebook', which_dim='all', cmap=None,
              col_sz=None):
